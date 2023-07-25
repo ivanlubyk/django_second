caa8919664411b83858e19845f83acf2f382ac0b
Іван Лубик <126516121+ivanlubyk@users.noreply.github.com>
2023-07-25 21:52:20 +0300

    Update models.py

--- sproba_6/hm_project/quotes/models.py
+++ sproba_6/hm_project/quotes/models.py
@@ -12,6 +12,9 @@ class Author(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    
+     def __str__(self):
+        return self.name
 
 class Quote(models.Model):
     quote = models.TextField()
